93573a99441afb498a1baf8a49a4a1f1edb635fa
vishnuveeraraghavan1905@gmail.com <vishnuveeraraghavan1905@gmail.com>
2024-04-10 16:08:03 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+// import './Components/Sample1'
+// import Sample1 from './Components/Sample1';
+// import './Components/sample2'
+// import Sample2 from './Components/sample2';
+// import './Components/Sample3'
+// import Sample3 from './Components/Sample3';
+// import './Components/Sample4'
+// import Sample4 from './Components/Sample4';
+// import './Components/Sample7'
+// import Sample7  from './Components/Sample7';
+// import './Components/Sample8'
+// import Sample8 from './Components/Sample8';
+// import './Components/Sample9'
+// import Sample9 from './Components/Sample9';
+// import Sample11 from './Components/Sample11';
+// import Sample19 from './Components/Sample19';
+import Sample20 from './Components/Sample20';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+  <div>
+    <Sample20/> 
+    {/* <Sample2/> */}
+  </div>
   );
 }
 
